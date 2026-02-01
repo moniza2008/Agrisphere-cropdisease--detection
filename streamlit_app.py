c70fc4fa09472311234293365e1012b56ce52fa7
moniza2008 <moniza2008@gmail.com>
2026-02-01 18:41:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,15 @@
 import streamlit as st
+from PIL import Image
 
 st.title("AgriSphere – Crop Disease Detection 🌱")
-st.write("Ideathon prototype is running successfully")
+st.write("Machine prototype is running successfully")
+
+uploaded_file = st.file_uploader(
+    "Upload a leaf image",
+    type=["jpg", "jpeg", "png"]
+)
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Leaf Image", use_column_width=True)
+    st.success("Image uploaded successfully!")
